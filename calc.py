d4a8542cd8f843f218d8eb3ff3ba7f6154f6e777
SigmaSigma234 <vectored101@outlook.com>
2024-09-24 12:35:19 +0100

    Add files via upload

---
+++ calc.py
@@ -0,0 +1,12 @@
+num1 = int(input("Please enter your first number: "))
+num2 = int(input("Please enter your first number: "))
+op = input("What operator would you like to use, +, -, * or /: ")
+if op == "+":
+    print(num1,"+",num2,"=",num1+num2)
+if op == "-":
+    print(num1,"-",num2,"=",num1-num2)
+if op == "*":
+    print(num1,"*",num2,"=",num1*num2)
+if op == "/":
+    print(num1,"/",num2,"=",num1/num2)
+
